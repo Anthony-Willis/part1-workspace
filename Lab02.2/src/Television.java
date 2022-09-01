638c2b1f9e898af978eb51d56e833adf4a6de950
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-01 15:35:01 -0700

    Initial commit

---
+++ Lab02.2/src/Television.java
@@ -0,0 +1,12 @@
+class Television {
+  String brand;
+  int volume;
+
+  void turnOn(){
+    System.out.println("The " + brand + " television is on, with a volume of " + volume);
+
+  }
+  void turnOff(){
+    System.out.println("The " + brand + " television is on off ");
+  }
+}
---
+++ Lab02.2/src/TelevisionClient.java
@@ -0,0 +1,18 @@
+public class TelevisionClient {
+
+  public static void main(String[] args) {
+    Television tv1 = new Television();
+    tv1.brand = "Sony";
+    tv1.volume = 25;
+    tv1.turnOn();
+    tv1.turnOff();
+
+    Television tv2 = new Television();
+    tv2.brand = "Logitech";
+    tv2.volume = 50;
+    tv2.turnOn();
+    tv2.turnOff();
+  }
+
+
+}
