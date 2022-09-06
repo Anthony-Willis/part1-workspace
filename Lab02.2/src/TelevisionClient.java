0bb4cc88f34540d750cf619edac4ba4332160a95
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-06 10:56:36 -0700

    added verifyInternetConnection.

--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -1,17 +1,23 @@
+import java.sql.SQLOutput;
+
 public class TelevisionClient {
 
   public static void main(String[] args) {
     Television tv1 = new Television();
-    tv1.brand = "Sony";
-    tv1.volume = 25;
+    tv1.setBrand("Sony") ;
+    tv1.setVolume(25);
     tv1.turnOn();
     tv1.turnOff();
 
     Television tv2 = new Television();
-    tv2.brand = "Logitech";
-    tv2.volume = 50;
+    tv2.setBrand("Logitech");
+    tv2.setVolume(50);
     tv2.turnOn();
     tv2.turnOff();
+
+
+    System.out.println(tv1.toString());
+    System.out.println(tv1);
   }
 
 
