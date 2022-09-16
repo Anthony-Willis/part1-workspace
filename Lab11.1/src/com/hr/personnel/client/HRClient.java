edc4a488e82804b3595cacc2aa2217df0efbd392
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-16 12:47:25 -0700

    Lab 11.1

---
+++ Lab11.1/src/com/hr/personnel/client/HRClient.java
@@ -0,0 +1,48 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+
+package com.hr.personnel.client;
+
+import com.hr.personnel.Department;
+import com.hr.personnel.Employee;
+import com.hr.personnel.HourlyEmployee;
+import com.hr.personnel.SalariedEmployee;
+import java.time.LocalDate;
+
+/**
+ * Application main-class.
+ */
+class HRClient {
+
+    public static void main(String[] args) {
+        // create Department object
+        Department dept = new Department("Sales", "Seattle");
+        System.out.println(dept);
+
+        // add Employees to it
+        dept.addEmployee(new HourlyEmployee("Jason", LocalDate.of(1990, 8, 24), 20, 40));
+        dept.addEmployee(new SalariedEmployee("Julie", LocalDate.of(2000, 2, 2), 110));
+        // TODO: 9/13/2022 make an hourly and salary employee by invoking addEmployee method
+        dept.addEmployee(new HourlyEmployee("Anthony", LocalDate.of(2022, 8, 29),20,15));
+        dept.addEmployee(new SalariedEmployee("Yaping", LocalDate.of(2021, 8, 29),180));
+        System.out.println(new HourlyEmployee("Anthony", LocalDate.of(2022, 8, 29),20,15));
+
+
+        // list its Employees
+        System.out.println("\nList employees:");
+        dept.listEmployees();
+
+        // make its Employees work
+        System.out.println("\nMake employees work:");
+        dept.workEmployees();
+
+        //pay its Employees
+        System.out.println("\npay its employees:");
+        dept.payEmployees();
+    }
+}
